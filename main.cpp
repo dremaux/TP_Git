a6db94347332eb18fb9bd7611c9c72fc46bac58a
ldremaux <ldremaux@btslivh.eu>
2021-01-07 18:46:48 +0100

    rajout du Cout "bonjour"

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 7 janvier 2021, 18:38
+ */
+
+#include <QApplication>
+#include <iostream> 
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    cout << "Boujour" << endl;
+    
+    // create and show your widgets here
+
+    return app.exec();
+}
